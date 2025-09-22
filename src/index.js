8a404668804c0f9466d7fcb635819c12c565d994
Umesh042005 <umeshdeshmukh926@gmail.com>
2025-09-22 10:44:15 +0530

    Ready for Vercel deploy

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import 'aos/dist/aos.css';
 import AOS from 'aos';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
